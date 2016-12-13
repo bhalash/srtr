7efb56fe083fc7bedcf6ac1a530fce40f7bf9cb4
Mark <mark@bhalash.com>
2016-12-13 22:01:35 +0000

    Added quicksort function.

--- srtr.js
+++ srtr.js
@@ -3,16 +3,73 @@
 const Srtr = function() {};
 
 /**
- * Bubble sort a collection.
+ * Private fallback predicate for bubble sort function.
+ *
+ * TODO: Explain the subtraction.
+ *
+ * @example
+ *
+ *      10 - 7  //  3
+ *      10 - 10 //  0
+ *      10 - 13 // -3
+ *
+ * @private
+ * @param {number} current
+ * @param {number} next
+ * @return {number} - Next value subtracted from current.
+ */
+
+function bubblePredicate(current, next) {
+    return current - next;
+}
+
+/**
+ * Use recursive quicksort to reorder a collection.
+ *
+ * TODO: Document this for newbies.
+ *
+ * @public
+ * @see https://en.wikipedia.org/wiki/Quicksort
+ * @param {array} collection - Unsorted collecton.
+ * @return {array} collection - Sorted collecton.
+ */
+
+Srtr.prototype.quicksort = function(collection) {
+    /* eslint no-magic-numbers: "off" */
+    if (collection.length < 2) {
+        return collection;
+    }
+
+    const pivot = collection[collection.length >> 1];
+
+    return [
+        ...this.quicksort(collection.filter(element => element < pivot)),
+        ...collection.filter(element => element === pivot),
+        ...this.quicksort(collection.filter(element => element > pivot))
+    ];
+};
+
+/**
+ * Use iterative bubble sort to reorder a collection.
+ *
+ * TODO: Document this for newbies.
+ * TODO: Document spaceship (<=>) operator.
+ *
+ * @example
+ *
+ *      2 <=> 3 // -1
+ *      3 <=> 3 //  0
+ *      3 <=> 2 //  1
  *
  * @public
  * @see https://en.wikipedia.org/wiki/Bubble_sort
- * @param {array} collection - Unsorted array.
+ * @see https://en.wikipedia.org/wiki/Three-way_comparison
+ * @param {array} collection - Unsorted collection
  * @param {function} predicate - Predicate function to test values.
- * @return {array} collection - Sorted arraylike object.
+ * @return {array} collection - Sorted collection.
  */
 
-Srtr.prototype.bubble = function(collection, predicate) {
+Srtr.prototype.bubblesort = function(collection, predicate = bubblePredicate) {
     collection = collection.slice();
 
     for (let index = 0; index < collection.length - 1; index++) {
