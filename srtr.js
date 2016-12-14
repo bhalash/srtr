445d779e221f8852f968171ea6725304da42a42b
Mark <mark@bhalash.com>
2016-12-14 06:35:25 +0000

    Made predicate generic to all public methods.

--- srtr.js
+++ srtr.js
@@ -3,9 +3,16 @@
 const Srtr = function() {};
 
 /**
- * Private fallback predicate for bubble sort function.
+ * Fallback sort predicate. Perform ternary comparsion of numbers.
  *
- * TODO: Explain the subtraction.
+ * TODO: Explain all this.
+ * TODO: Document spaceship (<=>) operator.
+ *
+ * @example
+ *
+ *      2 <=> 3 // -1
+ *      3 <=> 3 //  0
+ *      3 <=> 2 //  1
  *
  * @example
  *
@@ -14,13 +21,14 @@ const Srtr = function() {};
  *      10 - 13 // -3
  *
  * @private
- * @param {number} current
- * @param {number} next
+ * @see https://en.wikipedia.org/wiki/Three-way_comparison
+ * @param {number} value - Current value in collection.
+ * @param {number} comparison - Comparison value in collection.
  * @return {number} - Next value subtracted from current.
  */
 
-function bubblePredicate(current, next) {
-    return current - next;
+function spaceship(value, comparison) {
+    return value < comparison ? -1 : value === comparison ? 0 : 1;
 }
 
 /**
@@ -34,7 +42,7 @@ function bubblePredicate(current, next) {
  * @return {array} collection - Sorted collecton.
  */
 
-Srtr.prototype.quicksort = function(collection) {
+Srtr.prototype.quicksort = function(collection, predicate = spaceship) {
     /* eslint no-magic-numbers: "off" */
     if (collection.length < 2) {
         return collection;
@@ -43,9 +51,9 @@ Srtr.prototype.quicksort = function(collection) {
     const pivot = collection[collection.length >> 1];
 
     return [
-        ...this.quicksort(collection.filter(element => element < pivot)),
-        ...collection.filter(element => element === pivot),
-        ...this.quicksort(collection.filter(element => element > pivot))
+        ...this.quicksort(collection.filter(element => predicate(element, pivot) < 0), predicate),
+        ...collection.filter(element => predicate(element, pivot) === 0),
+        ...this.quicksort(collection.filter(element => predicate(element, pivot) > 0), predicate)
     ];
 };
 
@@ -53,23 +61,15 @@ Srtr.prototype.quicksort = function(collection) {
  * Use iterative bubble sort to reorder a collection.
  *
  * TODO: Document this for newbies.
- * TODO: Document spaceship (<=>) operator.
- *
- * @example
- *
- *      2 <=> 3 // -1
- *      3 <=> 3 //  0
- *      3 <=> 2 //  1
  *
  * @public
  * @see https://en.wikipedia.org/wiki/Bubble_sort
- * @see https://en.wikipedia.org/wiki/Three-way_comparison
  * @param {array} collection - Unsorted collection
  * @param {function} predicate - Predicate function to test values.
  * @return {array} collection - Sorted collection.
  */
 
-Srtr.prototype.bubblesort = function(collection, predicate = bubblePredicate) {
+Srtr.prototype.bubblesort = function(collection, predicate = spaceship) {
     collection = collection.slice();
 
     for (let index = 0; index < collection.length - 1; index++) {
